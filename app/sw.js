29879d08321ba0e1fe5a588152638b34b6d7369b
yankkhaing-watermelon <yankkhaing@gmail.com>
2026-08-30 20:40:58 +0800

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -10,7 +10,7 @@
  *      whole install and leaves the PREVIOUS worker in charge. Add files
  *      individually and tolerate misses.
  */
-const CACHE = "bmk-shell-v54";
+const CACHE = "bmk-shell-v55";
 const SHELL = ["./", "./index.html", "./app.js", "./charts.js", "./config.js",
                "./manifest.json"];
 
